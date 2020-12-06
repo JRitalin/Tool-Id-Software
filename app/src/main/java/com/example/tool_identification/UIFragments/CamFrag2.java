c628f10455d6a131c8f7fcd6c717365437061fd7
Daniel Leach <dleach2018@fau.edu>
2020-12-06 17:47:42 -0500

    change CamerView API to CameraX api

---
+++ app/src/main/java/com/example/tool_identification/UIFragments/CamFrag2.java
@@ -0,0 +1,268 @@
+package com.example.tool_identification.UIFragments;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.LayoutInflater;
+import android.view.OrientationEventListener;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageInfo;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.example.tool_identification.MainActivity;
+import com.example.tool_identification.R;
+import com.example.tool_identification.ToolIdentification.DetectObject;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.PictureResult;
+import com.otaliastudios.cameraview.controls.Mode;
+import com.otaliastudios.cameraview.frame.Frame;
+
+import org.jetbrains.annotations.NotNull;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.TensorImage;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
+
+public class CamFrag2 extends Fragment {
+
+    private static final String TAG = "Test";
+
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
+
+    Button btnCapture,btnBack;
+    ImageView imageView;
+    TextView feature,probability;
+    CameraView cameraView;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    DetectObject detectObject;
+    Context context;
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private Executor executor = Executors.newSingleThreadExecutor();
+    PreviewView previewView;
+    Bitmap bitmap;
+
+    public CamFrag2(){
+        // Required Empty Constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.frag_camera,container,false);
+
+        context = getContext();
+
+        imageView = view.findViewById(R.id.imageView);
+        feature = view.findViewById(R.id.feature);
+        probability = view.findViewById(R.id.probability);
+        btnCapture = view.findViewById(R.id.takePhotoButton);
+        btnCapture.setVisibility(View.VISIBLE);
+        btnBack = view.findViewById(R.id.returnButton);
+        btnBack.setVisibility(View.GONE);
+
+        surfaceView = view.findViewById(R.id.surfaceView);
+        surfaceView.setZOrderOnTop(true);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.setFormat(PixelFormat.TRANSPARENT);
+
+//        cameraView = view.findViewById(R.id.camera);
+//        cameraView.setMode(Mode.PICTURE);
+//        cameraView.setLifecycleOwner(this);
+
+        previewView = view.findViewById(R.id.previewView);
+
+        Log.d("Test","Ask for permission");
+        if (hasCameraPermission()) {
+            StartCamera();
+        } else {
+            requestPermission();
+        }
+
+
+        btnCapture.setOnClickListener(this::onClick);
+        btnBack.setOnClickListener(this::onClick);
+
+        return view;
+    }
+
+//    // take picture and listen for results
+//    public void takePicture(){
+//        cameraView.addCameraListener(new CameraListener() {
+//
+//            @Override
+//
+//            public void onPictureTaken(@NotNull PictureResult result) {
+//                result.toBitmap(320, 320, bitmap -> {
+//                    imageView.setImageBitmap(bitmap);
+//                    detectObject = new DetectObject(bitmap,surfaceHolder);
+//                    detectObject.setStream(false);
+//                    detectObject.setContext(context);
+//                    detectObject.Detect();
+//                    feature.setText(detectObject.getFeature());
+//                    probability.setText(detectObject.getFinalSize());
+//                    cameraView.close();
+//                });
+//            }
+//        });
+//        cameraView.takePicture();
+//    }
+
+
+
+    private void onClick(View v) {
+
+        TensorImage tensorImage = new TensorImage(DataType.UINT8);
+        tensorImage.load(bitmap);
+        imageView.setImageBitmap(bitmap);
+        detectObject = new DetectObject(bitmap,surfaceHolder);
+        detectObject.setStream(false);
+        detectObject.setContext(context);
+        detectObject.Detect();
+        feature.setText(detectObject.getFeature());
+        probability.setText(detectObject.getFinalSize());
+
+        btnCapture.setVisibility(View.GONE);
+        btnBack.setVisibility(View.VISIBLE);
+
+    }
+
+
+    private void StartCamera(){
+        cameraProviderFuture = ProcessCameraProvider.getInstance(context);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(getContext()));
+
+    }
+
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(1080,1440))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .setTargetRotation(Surface.ROTATION_270)
+                        .build();
+
+
+        imageAnalysis.setAnalyzer(executor, new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy image) {
+                int rotationDegrees = image.getImageInfo().getRotationDegrees();
+
+//                // insert your code here.
+                Log.d("Test","rotation Degrees: "+rotationDegrees);
+                bitmap = toBitmap(image.getImage());
+                Log.d("Test","Created bitmap");
+
+            }
+        });
+
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,imageAnalysis,preview);
+        Log.d("Test", "Bind successful");
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(
+                getContext(),
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(
+                getActivity(),
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
+    }
+
+
+
+    private Bitmap toBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 100, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+
+}
